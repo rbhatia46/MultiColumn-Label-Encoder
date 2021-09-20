63caa66a13bfbb4aca69f46f728ac849c127e02a
Rahul Bhatia <rbhatia46@gmail.com>
2021-09-20 17:30:20 +0530

    Initial Commit

---
+++ MultiColumnLabelEncoder.py
@@ -0,0 +1,129 @@
+class MultiColumnLabelEncoder(LabelEncoder):
+    """
+    Wraps sklearn LabelEncoder functionality for use on multiple columns of a
+    pandas dataframe.
+
+    """
+    def __init__(self, columns=None):
+        self.columns = columns
+
+    def fit(self, dframe):
+        """
+        Fit label encoder to pandas columns.
+
+        Access individual column classes via indexig `self.all_classes_`
+
+        Access individual column encoders via indexing
+        `self.all_encoders_`
+        """
+        # if columns are provided, iterate through and get `classes_`
+        if self.columns is not None:
+            # ndarray to hold LabelEncoder().classes_ for each
+            # column; should match the shape of specified `columns`
+            self.all_classes_ = np.ndarray(shape=self.columns.shape,
+                                           dtype=object)
+            self.all_encoders_ = np.ndarray(shape=self.columns.shape,
+                                            dtype=object)
+            for idx, column in enumerate(self.columns):
+                # fit LabelEncoder to get `classes_` for the column
+                le = LabelEncoder()
+                le.fit(dframe.loc[:, column].values)
+                # append the `classes_` to our ndarray container
+                self.all_classes_[idx] = (column,
+                                          np.array(le.classes_.tolist(),
+                                                  dtype=object))
+                # append this column's encoder
+                self.all_encoders_[idx] = le
+        else:
+            # no columns specified; assume all are to be encoded
+            self.columns = dframe.iloc[:, :].columns
+            self.all_classes_ = np.ndarray(shape=self.columns.shape,
+                                           dtype=object)
+            for idx, column in enumerate(self.columns):
+                le = LabelEncoder()
+                le.fit(dframe.loc[:, column].values)
+                self.all_classes_[idx] = (column,
+                                          np.array(le.classes_.tolist(),
+                                                  dtype=object))
+                self.all_encoders_[idx] = le
+        return self
+
+    def fit_transform(self, dframe):
+        """
+        Fit label encoder and return encoded labels.
+
+        Access individual column classes via indexing
+        `self.all_classes_`
+
+        Access individual column encoders via indexing
+        `self.all_encoders_`
+
+        Access individual column encoded labels via indexing
+        `self.all_labels_`
+        """
+        # if columns are provided, iterate through and get `classes_`
+        if self.columns is not None:
+            # ndarray to hold LabelEncoder().classes_ for each
+            # column; should match the shape of specified `columns`
+            self.all_classes_ = np.ndarray(shape=self.columns.shape,
+                                           dtype=object)
+            self.all_encoders_ = np.ndarray(shape=self.columns.shape,
+                                            dtype=object)
+            self.all_labels_ = np.ndarray(shape=self.columns.shape,
+                                          dtype=object)
+            for idx, column in enumerate(self.columns):
+                # instantiate LabelEncoder
+                le = LabelEncoder()
+                # fit and transform labels in the column
+                dframe.loc[:, column] =\
+                    le.fit_transform(dframe.loc[:, column].values)
+                # append the `classes_` to our ndarray container
+                self.all_classes_[idx] = (column,
+                                          np.array(le.classes_.tolist(),
+                                                  dtype=object))
+                self.all_encoders_[idx] = le
+                self.all_labels_[idx] = le
+        else:
+            # no columns specified; assume all are to be encoded
+            self.columns = dframe.iloc[:, :].columns
+            self.all_classes_ = np.ndarray(shape=self.columns.shape,
+                                           dtype=object)
+            for idx, column in enumerate(self.columns):
+                le = LabelEncoder()
+                dframe.loc[:, column] = le.fit_transform(
+                        dframe.loc[:, column].values)
+                self.all_classes_[idx] = (column,
+                                          np.array(le.classes_.tolist(),
+                                                  dtype=object))
+                self.all_encoders_[idx] = le
+        return dframe.loc[:, self.columns].values
+
+    def transform(self, dframe):
+        """
+        Transform labels to normalized encoding.
+        """
+        if self.columns is not None:
+            for idx, column in enumerate(self.columns):
+                dframe.loc[:, column] = self.all_encoders_[
+                    idx].transform(dframe.loc[:, column].values)
+        else:
+            self.columns = dframe.iloc[:, :].columns
+            for idx, column in enumerate(self.columns):
+                dframe.loc[:, column] = self.all_encoders_[idx]\
+                    .transform(dframe.loc[:, column].values)
+        return dframe.loc[:, self.columns].values
+
+    def inverse_transform(self, dframe):
+        """
+        Transform labels back to original encoding.
+        """
+        if self.columns is not None:
+            for idx, column in enumerate(self.columns):
+                dframe.loc[:, column] = self.all_encoders_[idx]\
+                    .inverse_transform(dframe.loc[:, column].values)
+        else:
+            self.columns = dframe.iloc[:, :].columns
+            for idx, column in enumerate(self.columns):
+                dframe.loc[:, column] = self.all_encoders_[idx]\
+                    .inverse_transform(dframe.loc[:, column].values)
+        return dframe.loc[:, self.columns].values
